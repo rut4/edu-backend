96ce237575bb0b7935484b3a0f10d44451a31882
Eduard <rut4ik@gmail.com>
2014-01-14 18:13:31 +0400

    Add database dump

---
+++ student_db.sql
@@ -0,0 +1,414 @@
+-- MySQL dump 10.13  Distrib 5.5.34, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: student
+-- ------------------------------------------------------
+-- Server version	5.5.34-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `addresses`
+--
+
+DROP TABLE IF EXISTS `addresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `addresses` (
+  `address_id` int(11) NOT NULL AUTO_INCREMENT,
+  `city_id` int(11) DEFAULT NULL,
+  `region_id` int(11) DEFAULT NULL,
+  `postal_code` int(11) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `home_number` varchar(10) DEFAULT NULL,
+  `flat` int(4) DEFAULT NULL,
+  PRIMARY KEY (`address_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `addresses`
+--
+
+LOCK TABLES `addresses` WRITE;
+/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
+INSERT INTO `addresses` VALUES (1,1,1,123123,'qeqwe','123',123),(2,1,1,123123,'qeqwe','123',123);
+/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admins` (
+  `admin_id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+INSERT INTO `admins` VALUES (0,'admin','21232f297a57a5a743894a0e4a801fc3');
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cities` (
+  `city_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `region_id` int(11) DEFAULT NULL,
+  `shipping_price` int(7) DEFAULT NULL,
+  PRIMARY KEY (`city_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cities`
+--
+
+LOCK TABLES `cities` WRITE;
+/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+INSERT INTO `cities` VALUES (1,'Taganrog',1,NULL),(2,'Toronto',2,NULL);
+/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `customer_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `password` varchar(33) NOT NULL,
+  `rating` decimal(10,2) DEFAULT '0.00',
+  `login` varchar(16) NOT NULL,
+  `quote_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'test1','ad0234829205b9033196ba818f7a872b',0.00,'',NULL),(2,'test2','8ad8757baa8564dc136c1e07507f4a98',0.00,'test1',NULL),(3,'qwe','76d80224611fc919a5d54f0ff9fba446',0.00,'qwe',NULL),(5,'qwe1','a51c008b213c7a0462e49895df164b50',0.00,'qwe1',NULL),(7,'qwe2','516b5d9924c123fecc22e0eff6c3e179',0.00,'qwe2',NULL),(8,'qwe4','805bbf75b0741860d860dac3dad9ca49',0.00,'qwe4',NULL),(9,'q','f09564c9ca56850d4cd6b3319e541aee',0.00,'q',NULL),(10,'q2','74d502a7131cdac90eecdfb0531c4e87',0.00,'q2',NULL),(11,'asd','7815696ecbf1c96e6894b779456d330e',0.00,'asd',NULL),(15,'asd','7815696ecbf1c96e6894b779456d330e',0.00,'Ñ„Ñ‹Ð²',NULL),(16,'asd','7815696ecbf1c96e6894b779456d330e',0.00,'qwertyu',NULL),(17,'q','7694f4a66316e53c8cdd9d9954bd611d',0.00,'qwertyuytrewq',NULL),(18,'1','c4ca4238a0b923820dcc509a6f75849b',0.00,'notguest',NULL),(19,'zz','25ed1bcb423b0b7200f485fc5ff71c8e',0.00,'zz',NULL),(20,'dic','470dcd7b646bfcf16d7f0e85bd145ac4',0.00,'dic',NULL),(21,'dic1','e6b96b03d5c4b094770101d09e2f5fe5',0.00,'dic1',NULL),(22,'dic2','1867d163388e441834a0ad05eea997dc',0.00,'dic2',NULL),(23,'dic3','341008a26b505f3a2244ba2b03cb2a99',0.00,'dic3',NULL),(24,'dic4','188defe3324a72bd3aa3ba9a7ee91254',0.00,'dic4',NULL),(25,'test1','28b662d883b6d76fd96e4ddc5e9ba780',0.00,'tes',NULL);
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_items`
+--
+
+DROP TABLE IF EXISTS `order_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_items` (
+  `order_item_id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `quantity` int(7) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `sku` int(11) NOT NULL,
+  `cost` int(7) NOT NULL,
+  PRIMARY KEY (`order_item_id`),
+  UNIQUE KEY `sku` (`sku`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_items`
+--
+
+LOCK TABLES `order_items` WRITE;
+/*!40000 ALTER TABLE `order_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_products`
+--
+
+DROP TABLE IF EXISTS `order_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_products` (
+  `link_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) unsigned NOT NULL,
+  `order_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`link_id`),
+  KEY `product_id` (`product_id`),
+  KEY `order_id` (`order_id`),
+  CONSTRAINT `order_products_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `order_products_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_products`
+--
+
+LOCK TABLES `order_products` WRITE;
+/*!40000 ALTER TABLE `order_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `order_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `grand_total` decimal(10,2) DEFAULT NULL,
+  `customer_id` int(11) unsigned NOT NULL,
+  `seller_id` int(11) unsigned NOT NULL,
+  `shipping_method_code` varchar(255) DEFAULT NULL,
+  `payment_method_code` varchar(255) DEFAULT NULL,
+  `shipping` decimal(5,2) DEFAULT NULL,
+  `subtotal` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `CUSTOMER_ID_INDEX` (`customer_id`),
+  KEY `seller_id` (`seller_id`),
+  CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`seller_id`) REFERENCES `sellers` (`seller_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'2013-11-20 14:19:38',100.00,1,1,NULL,NULL,NULL,NULL),(3,'2013-11-20 14:19:54',1400.20,2,1,NULL,NULL,NULL,NULL),(4,'2013-11-20 14:20:05',400.00,3,1,NULL,NULL,NULL,NULL),(5,'2013-11-20 14:20:19',10.00,1,2,NULL,NULL,NULL,NULL),(6,'2013-11-20 14:23:44',10000.00,1,2,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `product_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `sku` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `special_price` decimal(10,2) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  UNIQUE KEY `SKU_INDEX` (`sku`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Nokla','123123',0.00,NULL,NULL),(2,'lPhone','111',0.00,NULL,NULL),(4,'Nokla 2',NULL,0.00,NULL,NULL),(6,'Sumsung','234628374',100.00,99.00,'http://citrade.ru/UserFiles/Image/img4018_30795s.jpg'),(7,'YoPhone','324234',1000.00,0.00,'http://icdn2.digitaltrends.com/image/yota-yotaphone-alternate-200x200-c.jpg'),(8,'Lumia 2520','34523352323',1000.00,999.99,'http://www.prostomac.com/wp-content/uploads/2013/11/Nokia-Lumia-1520-Lumia-2520-official-hands-on-videos-are-out.jpg'),(9,'Xperia Z','202398423423',900.00,NULL,'http://nsk-digital.ru/published/publicdata/Z59983NSK/attachments/SC/products_pictures/sony-xperia-z-c6602-belyj-1_thm.jpg'),(11,'rug','2342322222',10.00,9.99,'http://domashnih-usloviyah.ru/wp-content/uploads/2013/08/pled.jpg'),(12,'lPod','123456765',4.00,NULL,'http://av-tehnika.ru/images/product_images/info_images/190_7.jpg');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quote`
+--
+
+DROP TABLE IF EXISTS `quote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quote` (
+  `quote_item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `session_id` varchar(40) DEFAULT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(7) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`quote_item_id`),
+  UNIQUE KEY `customer_product` (`customer_id`,`product_id`),
+  UNIQUE KEY `session_product` (`session_id`,`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quote`
+--
+
+LOCK TABLES `quote` WRITE;
+/*!40000 ALTER TABLE `quote` DISABLE KEYS */;
+INSERT INTO `quote` VALUES (86,20,NULL,9,1),(87,20,NULL,11,1),(96,22,NULL,9,1),(97,22,NULL,6,1),(98,NULL,'l31gctukcj86ttp7v44qnf12i6',11,2);
+/*!40000 ALTER TABLE `quote` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quote_items`
+--
+
+DROP TABLE IF EXISTS `quote_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quote_items` (
+  `quote_item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `quote_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(7) NOT NULL,
+  PRIMARY KEY (`quote_item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quote_items`
+--
+
+LOCK TABLES `quote_items` WRITE;
+/*!40000 ALTER TABLE `quote_items` DISABLE KEYS */;
+INSERT INTO `quote_items` VALUES (1,1,8,2);
+/*!40000 ALTER TABLE `quote_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quotes`
+--
+
+DROP TABLE IF EXISTS `quotes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quotes` (
+  `quote_id` int(11) NOT NULL AUTO_INCREMENT,
+  `shipping_method_code` varchar(25) DEFAULT NULL,
+  `address_id` int(11) DEFAULT NULL,
+  `payment_method_code` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`quote_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quotes`
+--
+
+LOCK TABLES `quotes` WRITE;
+/*!40000 ALTER TABLE `quotes` DISABLE KEYS */;
+INSERT INTO `quotes` VALUES (1,'table_rate',1,'cash_on_delivery'),(2,'fixed',18,'cash_on_delivery'),(3,'table_rate',19,'cash_on_delivery'),(4,'table_rate',21,'cash_on_delivery');
+/*!40000 ALTER TABLE `quotes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `regions`
+--
+
+DROP TABLE IF EXISTS `regions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `regions` (
+  `region_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`region_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `regions`
+--
+
+LOCK TABLES `regions` WRITE;
+/*!40000 ALTER TABLE `regions` DISABLE KEYS */;
+INSERT INTO `regions` VALUES (1,'Rostovskaya oblast'),(2,'Ontario');
+/*!40000 ALTER TABLE `regions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reviews` (
+  `review_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) DEFAULT NULL,
+  `email` varchar(25) DEFAULT NULL,
+  `text` text,
+  `rating` int(1) unsigned DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`review_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reviews`
+--
+
+LOCK TABLES `reviews` WRITE;
+/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
+INSERT INTO `reviews` VALUES (1,'Vasya','a@a.ru','Very good',4,1),(2,'Igor','b@gmail.com','nice',5,2),(3,'Petr','q@q.ru','it is very intresting',1,7),(4,'Test1','q@q.ru','Test1Text',5,7),(5,'Test2','q@q.ru','Test2Text',5,7),(6,'Test3','q@q.ru','Test3Text',5,7),(7,'Test4','q@q.ru','Test4Text',5,7),(8,'Test5','q@q.ru','Test5Text',5,7),(9,'arrrrgh','g@g.g','i\'m tired',1,2),(10,'arrrrgh','g@g.g','i made mistake',5,2),(11,'qww','qw@www','sss',4,11);
+/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sellers`
+--
+
+DROP TABLE IF EXISTS `sellers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sellers` (
+  `seller_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `city` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `comission` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`seller_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sellers`
+--
+
+LOCK TABLES `sellers` WRITE;
+/*!40000 ALTER TABLE `sellers` DISABLE KEYS */;
+INSERT INTO `sellers` VALUES (1,'Igor','Izhevsk',0.30),(2,'Nastya','Taganrog',0.50),(3,'Vova','Moskva',0.80),(4,'Ivan','Kazan',0.40);
+/*!40000 ALTER TABLE `sellers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-01-14 14:12:59
